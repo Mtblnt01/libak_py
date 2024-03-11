31294ba762ba6c0cca343d844ca189d769261e58
Mtblnt01 <126462765+Mtblnt01@users.noreply.github.com>
2024-03-11 12:07:31 +0100

    a feladat megoldása

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -17,12 +17,32 @@ namespace ConsoleApp1
             }
             Console.WriteLine();
         }
+        static int Osszegez(int[] t)
+        {
+            int osszeg = 0;
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (t[i] <= 3)
+                {
+                    osszeg += t[i];
+                };
+            };
+            return osszeg;
+        }
+        static void Kiir_eredmenyek(int roka_liba_suly)
+        {
+            Console.WriteLine($"A róka libáinak súlya {roka_liba_suly}");
+        }
+
 
         static void Main(string[] args)
         {
             int[] libak = { 1, 5, 2, 3, 4 };
             Kiir(libak, "libak sullyai" );
-
+            int hany_kilo_libat_ehet_meg_a_roka = Osszegez(libak);
+            Kiir_eredmenyek(hany_kilo_libat_ehet_meg_a_roka);
+            
+            
             Console.WriteLine("Nyomj eg billentyűt");
             Console.ReadKey();
         }
