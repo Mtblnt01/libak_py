5896c6785adc0ecdaf5500054ecd4abc4a911feb
Mtblnt01 <126462765+Mtblnt01@users.noreply.github.com>
2024-03-11 11:48:39 +0100

    tomb kiirva

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Kiir(int[] t, string cim)
+        {
+            Console.WriteLine(cim);
+            foreach (int szam in t)
+            {
+                Console.Write($"{szam}, ");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            int[] libak = { 1, 5, 2, 3, 4 };
+            Kiir(libak, "libak sullyai" );
+
+            Console.WriteLine("Nyomj eg billentyűt");
+            Console.ReadKey();
+        }
+    }
+}
